d225d2a34ad758298472a74afe598425afe3d825
Jacob Archambault <jacob.archambault@waystar.com>
2021-03-06 09:06:02 -0500

    Prefix unused variable in index.js with underscore

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();                  // create the express app
 
-app.use('/', function(req, res) {     // when the default route is requested...
+app.use('/', function(_req, res) {     // when the default route is requested...
     res.send('Hello World!');         // call this function and send a response
 });
 
