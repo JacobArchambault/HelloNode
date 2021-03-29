ad9489e144ebfd16a27870c60c88fe8535521bec
Jacob Archambault <jacob.archambault@waystar.com>
2021-03-28 23:23:33 -0400

    Update to ES6 module

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-var express = require('express');
-var app = express();                  // create the express app
+import express from 'express';
+let app = express();                  // create the express app
 
-app.use('/', function(_req, res) {     // when the default route is requested...
-    res.send('Hello World!');         // call this function and send a response
-});
+app.use('/', (_req, res) =>      // when the default route is requested...
+    res.send('Hello World!')         // call this function and send a response
+);
 
-app.listen(3000, function() {
+app.listen(3000, function () {
     console.log('Listening on port 3000, ctrl-c to quit.');
 });
